9c8a0f14130afc1a688fc13c23505f2f7e7905c9
Jonas Brandvik <timberbain@gmail.com>
2016-08-26 09:24:56 +0200

    Started working on 'Create Item'

---
+++ web/client/js/fridgemenu.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+
+
+
+export class InputForm extends React.Component {
+    render() {
+        return (
+            <div className="row" style={{'lineHeight': '10px'}}>
+                <div className="input-field col black-text">
+                    <i className="material-icons prefix">text_fields</i>
+                    <input id="icon_prefix" type="text" className="validate" />
+                    <label htmlFor="icon_prefix">Item Name</label>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+export class DropDownItem extends React.Component {
+    render(){
+        return (
+            <li><a className="blue-text" href="#!" onClick={this.props.onClick}>{this.props.children}</a></li>
+        )
+    }
+}
+
+
+/* Navigation bar on the left side */
+export class ControlMenu extends React.Component {
+    //@ Override
+    constructor(props){
+        super(props);
+        this.state = {
+            selectedItem: {id: -1, name: ""},
+            items: [
+                {id: 0, name: "Mjölk"},
+                {id: 1, name: "Smör"},
+                {id: 2, name: "Socker"}
+            ],
+        };
+    }
+
+    selectItem(item){
+        this.setState({selectedItem: item});
+    }
+    resetItem(){
+        this.selectItem({id:-1, name:""});
+    }
+
+    itemDropdown(name){
+        if(name == ""){
+            return <i className="material-icons">keyboard_arrow_down</i>
+        } else {
+            return <i className="material-icons">keyboard_arrow_right</i>
+        }
+    }
+    makeButton(icon, color, callback, enabled){
+        return (
+            <a className={"menu-button btn-floating btn-large " + color + " center " + (enabled ? "waves-effect waves-light" : "disabled")} onClick={ enabled ? callback : function(){}}>
+                <i className="material-icons">{icon}</i>
+            </a>
+        )
+    }
+
+    render() {
+        return (
+            <ul id="slide-out" className="side-nav" style={{transform: 'translateX(-100%)'}}>
+                <li>
+                    <div className="center blue-text lighten-1">
+                        <h5>Menu</h5>
+                    </div>
+                </li>
+                <li className="alt_divider center"></li>
+                <li>
+                    <div className="container center blue-text lighten-1" style={{'lineHeight': '10px'}}>
+                        Add
+                        <ul id="available-items" className="dropdown-content">
+                            {this.state.items.map(function(e, i){
+                                return <DropDownItem key={"mfid_" + i} itemname={e.name} itemid={e.id} onClick={function(){ this.selectItem(e); }.bind(this)}>{e.name}</DropDownItem>
+                            }.bind(this))}
+
+                        </ul>
+                        <a className="btn dropdown-button white blue-text lighten-1" href="#!" data-activates="available-items">
+                            {this.itemDropdown(this.state.selectedItem.name)}
+                            <span className="dropdown-item-name">{this.state.selectedItem.name}</span>
+
+                        </a>
+                        <div className="menu-controls center">
+                            {this.makeButton("add_shopping_cart", "green", function(){}, true)}
+                            {this.makeButton("cached", "orange", this.resetItem.bind(this), this.state.selectedItem.id != -1)}
+                        </div>
+                    </div>
+                </li>
+                <li className="alt_divider center"></li>
+                <li>
+                    <div className="center blue-text lighten-1">
+                        Create
+                    </div>
+                    <InputForm></InputForm>
+                </li>
+            </ul>
+        )
+
+    }
+}
--- web/client/js/index.jsx
+++ web/client/js/index.jsx
@@ -7,11 +7,14 @@ import React from 'react'
 import {render} from 'react-dom'
 
 import {FridgeItems, FridgeItem} from './fridgeitems'
+import {ControlMenu} from './fridgemenu'
 
 $(document).ready( init );
 
 function init() {
     $(".button-collapse").sideNav();
+    Materialize.updateTextFields();
+
     // $('.collapsible').collapsible();
 
 }
@@ -64,106 +67,24 @@ function init() {
 /* React approach */
 (function(){
 
-    class LeftMenuItem extends React.Component {
-        render(){
-            return (
-                <li><a className="blue-text" href="#!" onClick={this.props.onClick}>{this.props.children}</a></li>
-            )
-        }
-    }
-
-    class LeftMenu extends React.Component {
-        //@ Override
-        constructor(props){
-            super(props);
-            this.state = {
-                selectedItem: {id: -1, name: ""},
-                items: [
-                    {id: 0, name: "Mjölk"},
-                    {id: 1, name: "Smör"},
-                    {id: 2, name: "Socker"}
-                ],
-            };
-        }
-
-        selectItem(item){
-            this.setState({selectedItem: item});
-        }
-        resetItem(){
-            this.selectItem({id:-1, name:""});
-        }
-
-        itemDropdown(name){
-            if(name == ""){
-                return <i className="material-icons">keyboard_arrow_down</i>
-            } else {
-                return <i className="material-icons">keyboard_arrow_right</i>
-            }
-        }
-        makeButton(icon, color, callback, enabled){
-            return (
-                <a className={"menu-button btn-floating btn-large " + color + " center " + (enabled ? "waves-effect waves-light" : "disabled")} onClick={ enabled ? callback : function(){}}>
-                    <i className="material-icons">{icon}</i>
-                </a>
-            )
-        }
-
-        render() {
-            return (
-                <ul id="slide-out" className="side-nav" style={{transform: 'translateX(-100%)'}}>
-                    <li>
-                        <div className="center blue-text lighten-1">
-                            <h5>Menu</h5>
-                        </div>
-                    </li>
-                    <li className="alt_divider center"></li>
-                    <li>
-                        <div className="center blue-text lighten-1">
-                            Add
-                            <ul id="available-items" className="dropdown-content">
-                                {this.state.items.map(function(e, i){
-                                    return <LeftMenuItem key={"mfid_" + i} itemname={e.name} itemid={e.id} onClick={function(){ this.selectItem(e); }.bind(this)}>{e.name}</LeftMenuItem>
-                                }.bind(this))}
-
-                            </ul>
-                            <a className="btn dropdown-button white blue-text lighten-1" href="#!" data-activates="available-items">
-                                {this.itemDropdown(this.state.selectedItem.name)}
-                                <span className="dropdown-item-name">{this.state.selectedItem.name}</span>
-
-                            </a>
-                            <div className="menu-controls center">
-                                {this.makeButton("add_shopping_cart", "green", function(){}, true)}
-                                {this.makeButton("cached", "orange", this.resetItem.bind(this), this.state.selectedItem.id != -1)}
-                            </div>
-                        </div>
-                    </li>
-                    <li className="alt_divider center"></li>
-                    <li>
-                        <div className="center blue-text lighten-1">
-                            Create
-                        </div>
-                    </li>
-                </ul>
-            )
-
-        }
-    }
 
     class Main extends React.Component {
         render() {
             return (
-                <nav className="blue lighten-1">
-                    <div className="nav-wrapper container">
-                        <LeftMenu url={this.props.url}></LeftMenu>
-                        <a className="right brand-logo" href="#">
-                            <img className="header-icon responsive-img" src="images/bfridge_icon.png" />
-                        </a>
-                        <a href="#" data-activates="slide-out" className="button-collapse show-on-large">
-                            <i className="material-icons">menu</i>
-                        </a>
-                    </div>
+                <div>
+                    <nav className="blue lighten-1">
+                        <div className="nav-wrapper container">
+                            <a className="right brand-logo" href="#">
+                                <img className="header-icon responsive-img" src="images/bfridge_icon.png" />
+                            </a>
+                            <a href="#" data-activates="slide-out" className="button-collapse show-on-large">
+                                <i className="material-icons">menu</i>
+                            </a>
+                        </div>
+                    </nav>
+                    <ControlMenu url={this.props.url}></ControlMenu>
                     <FridgeItems url={this.props.url}/>
-                </nav>
+                </div>
             )
         }
     }
